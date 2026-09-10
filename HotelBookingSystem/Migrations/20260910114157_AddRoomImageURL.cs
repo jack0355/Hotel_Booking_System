32a77988d28bc90678760b04095df9dfcca9384f
jack0355 <zramadan944@gmail.com>
2026-09-10 03:44:21 -0800

    Add Missing AddRoomImageURL migration

---
+++ HotelBookingSystem/Migrations/20260910114157_AddRoomImageURL.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBookingSystem.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoomImageURL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Rooms",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Rooms");
+        }
+    }
+}
